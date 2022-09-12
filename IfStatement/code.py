563a438778eb7bc2acf4df0868243515af621f49
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-09-12 19:41:37 +0200

    Learning IfStatement

---
+++ IfStatement/code.py
@@ -0,0 +1,12 @@
+day_of_the_week = input("What day of the week is it today?: >>>> ").lower()
+
+# print(day_of_the_week=="Monday")
+
+if day_of_the_week == "monday":
+    print(f"Have a great start to your week today is {day_of_the_week}")
+elif day_of_the_week == "Tuesday":
+    print("Today we have Tuesday great day for coding ")
+else:
+    print("For sure today is not a Monday ;P")
+
+print("This allways prints STH")
