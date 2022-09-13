549eb2ef197da7b88c0b97411fa088c96348ec55
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-09-13 16:54:08 +0200

    Create Get and post endpoints

--- Api2/code/app.py
+++ Api2/code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -6,10 +6,30 @@ app = Flask(__name__)
 # api works with resources and every resource works with the class
 api = Api(app)
 
-class Student(Resource):
+items =[]
+
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
+    
 
-api.add_resource(Student, '/student/<string:name>')
+    def post(self,name):
+        # If the request doesn't attach the json payload or the request doesnt have the proper Content-type header request.get_json() ll give an error
+        # force=True means we don't need content type header
+        #data ll be a dictionary 
+        data = request.get_json(force=True)
+        item = {'name':name, 'price': data['price']}
+        items.append(item)
+        return item, 201
 
-app.run(port=5000)
+class ItemList(Resource):
+    def get(self):
+        return{'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug = True)
